e86778330621068ee999124c243ca0cb2c4d9d25
Long <infz3r0@gmail.com>
2018-03-22 20:26:01 +0700

    Default

---
+++ NPL/NPL/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NPL.Models;
+
+namespace NPL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBNPLDataContext data = new DBNPLDataContext();
+
+        public ActionResult Index()
+        {
+            Admin r = data.Admins.SingleOrDefault(i => i.Username == "admin");
+            return View(r);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
