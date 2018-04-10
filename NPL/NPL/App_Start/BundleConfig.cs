e717477f538a28a8c88915f2d2ad111a94b75c6a
Nguyen Phu <nguyenphuocphu@outlook.com>
2018-04-10 23:31:35 +0700

    2

--- NPL/NPL/App_Start/BundleConfig.cs
+++ NPL/NPL/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace NPL
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style_user.css"));
         }
     }
 }
