118ba4dbec790051a2466b95d594bbb639e0f1b6
Nguyen Phu <nguyenphuocphu@outlook.com>
2018-04-13 22:13:44 +0700

    .

--- NPL/NPL/App_Start/BundleConfig.cs
+++ NPL/NPL/App_Start/BundleConfig.cs
@@ -9,15 +9,11 @@ namespace NPL
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.3.1.min.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-confirmation").Include(
-                        "~/Scripts/bootstrap-confirmation.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
-                        "~/Scripts/umd/popper.min.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
